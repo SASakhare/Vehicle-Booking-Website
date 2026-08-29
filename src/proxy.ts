72c640b9daea10136777b1c089a0a48f9847b29d
sejal <sakharwsejal@gmail.com>
2026-08-29 10:00:41 +0530

    implement middleware/proxy

---
+++ src/proxy.ts
@@ -0,0 +1,92 @@
+import { NextRequest, NextResponse } from "next/server"
+import { auth } from "./auth";
+
+
+const PUBLIC_ROUTES = ["/"]
+
+const PUBLIC_APIS = ["/api/auth"]
+
+
+export async function proxy(req: NextRequest) {
+
+    const { pathname } = req.nextUrl
+
+    console.log(pathname);
+
+    if (
+        pathname.startsWith("/_next") ||
+        pathname.startsWith("/favicon.ico") ||
+        pathname.startsWith(".")
+    ) {
+        return NextResponse.next();
+    }
+
+    if (PUBLIC_ROUTES.includes(pathname)) {
+        return NextResponse.next();
+    }
+
+    if (PUBLIC_APIS.includes(pathname)) {
+        return NextResponse.next();
+    }
+
+    const session = await auth();
+
+    if (!session) {
+        return NextResponse.redirect(new URL("/", req.url))
+    }
+
+    if (pathname.startsWith("/api")) {
+        if (!session.user) {
+            return Response.json({
+                message: "unauthorised acess"
+            }, {
+                status: 401
+            });
+        }
+    }
+
+    const role = session.user?.role
+
+    if (pathname.startsWith("/admin")) {
+        if (role != 'admin') {
+            return NextResponse.redirect(new URL("/", req.url))
+        }
+
+    }
+
+    if (pathname.startsWith("/partner")) {
+        if (role != 'partner') {
+            return NextResponse.redirect(new URL("/", req.url))
+        }
+    }
+
+    return NextResponse.next();
+
+}
+
+
+export const config={
+    matcher:['/((?!_next/static|_next/image|favicon.ico).*)'],
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
